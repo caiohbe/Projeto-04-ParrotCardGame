6f0b9ee11e023074784aa8c39428fcb593968749
caiohbe <caiohbe@gmail.com>
2022-08-17 12:52:54 -0300

    Layout temp

---
+++ script.js
@@ -0,0 +1,9 @@
+// let qtt = prompt('Com quantas cartas você deseja jogar? (4-14 apenas par)')
+// while (true) {
+//     if (qtt < 4 || qtt > 14 || qtt % 2 !== 0) {
+//         qtt = prompt('Com quantas cartas você deseja jogar? (4-14 apenas par)')
+//     } else {
+//         break
+//     }  
+
+// }
