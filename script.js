75d9bfa4863934b61a23d0b81a97d808cbfbc04f
caiohbe <caiohbe@gmail.com>
2022-08-18 23:40:58 -0300

    dynamic HTML + cardflip

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ while (qtt < 4 || qtt > 14 || qtt % 2 !== 0) {
 }
 
 
-const parrots = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif'] 
+const parrots = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'] 
 
 function comparador() { 
 	return Math.random() - 0.5; 
@@ -19,10 +19,19 @@ for(let i = 0; i < qtt/2; i++) {
 }
 
 game.sort(comparador)
-console.log(game)
 
-const parrotList = document.querySelectorAll("span")
+const parrotList = document.querySelector(".cards")
 for(let i = 0; i < qtt; i++) {
-    parrotList[i].innerHTML = `<img src="imgs/${game[i]}" alt="">`
-    parrotList[i].classList.add('unhidden')
+    parrotList.innerHTML += `
+    <span><div onclick="turn(this)"><img src="imgs/parrot.png" alt=""></div><div class="unturned"><img src="imgs/${game[i]}" alt=""></div></span>
+    `
+}
+
+function turn(isso) {
+    console.log(isso)
+    isso.classList.add('turn')
+
+    adjacent = isso.nextElementSibling
+    console.log(adjacent)
+    adjacent.classList.add('turned')
 }
